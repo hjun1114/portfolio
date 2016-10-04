8c804cd88589496cdf46501c5ac99ad71704a5a7
hjun1114 <hjun1114@gmail.com>
2016-10-03 21:50:34 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+  $(function(){
+
+      $(".dark-overlay").addClass('animated fadeInLeft');
+
+      $(".header-tagline").addClass('animated fadeIn');
+
+      $("#fullpage").fullpage({
+         anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage']
+      });
+  });
